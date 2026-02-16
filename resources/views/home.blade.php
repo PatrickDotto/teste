85125797da1739fe48b84c3a44dbcffb05dcde6f
Patrick Dotto <patrickdotto05@gmail.com>
2026-02-16 15:31:25 -0300

    Main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main_layout')
+
+@section('titulo', 'Página Home')
+
+@section('content')
+    <div class="container mt-5">
+        <div class="col card p-5 bg-secondary text-center text-white">
+            <body class="bg-dark">
+                ooo
+            </body>
+        </div>
+    </div> 
+    <div class="container mt-3">
+        <div class="col card p-4 bg-white text-grey text-center">
+            dassdsa
+        </div>
+    </div>
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,8 @@
+<div class="fluid bg-black text-center">
+    <div class="row text-grey">
+        <div class="col p-3">
+            <h1>Barra de topo</h1>
+
+        </div>
+    </div>
+</div>
