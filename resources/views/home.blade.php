b5449c6ae5e9da6b7cbd56e0990c29876d96ed58
Patrick Dotto <patrickdotto05@gmail.com>
2026-02-16 16:14:58 -0300

    Criação do MyComponent

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,9 @@
 @extends('layouts.main_layout')
-
-@section('titulo', 'Página Home')
-
 @section('content')
-    <div class="container mt-5">
-        <div class="col card p-5 bg-secondary text-center text-white">
-            <body class="bg-dark">
-                ooo
-            </body>
-        </div>
-    </div> 
-    <div class="container mt-3">
-        <div class="col card p-4 bg-white text-grey text-center">
-            dassdsa
-        </div>
-    </div>
+<div class="container bg-secondary text-white p-5">
+    <h3>Este texto faz parte da view</h3>
+</div>
+
+<x-my-component />
+
 @endsection
